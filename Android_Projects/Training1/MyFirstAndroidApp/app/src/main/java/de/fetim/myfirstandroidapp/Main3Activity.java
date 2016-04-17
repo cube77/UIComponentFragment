f52656eccfcfa760521f49c4892c71b18031a5f3
Fetim <fazlija@fetim.de>
2016-04-17 21:22:00 +0200

    added finish activity button on activity screen 3 added in xml button4 "Activity beenden"

--- Android_Projects/Training1/MyFirstAndroidApp/app/src/main/java/de/fetim/myfirstandroidapp/Main3Activity.java
+++ Android_Projects/Training1/MyFirstAndroidApp/app/src/main/java/de/fetim/myfirstandroidapp/Main3Activity.java
@@ -1,11 +1,13 @@
 package de.fetim.myfirstandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -15,6 +17,14 @@ public class Main3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ((Button)findViewById(R.id.button4)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
